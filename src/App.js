9ed55e180c6315598dcae8dd9f963d9d98353c29
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-01-27 11:45:29 -0300

    added structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./Header.js";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
-
-export default App;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
