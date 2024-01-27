2871d22022ecbb81ab536c931d6adc2bd7628ab3
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-01-27 12:52:37 -0300

    added main to display something

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+function Nav() {
+  return (
+    <article>
+      <h1>Little Lemon</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        necessitatibus doloremque reiciendis enim recusandae soluta harum sed,
+        nostrum quibusdam, repellat deserunt alias, nihil aliquid quod!
+        Excepturi perferendis tenetur rem provident alias fugit consequuntur,
+        quam itaque eveniet eum quidem quibusdam id, ab vitae, voluptate amet
+        delectus deserunt? Eius voluptates dolore nobis quis? Ipsum ab eum
+        omnis! Maxime nemo veritatis nisi asperiores possimus pariatur ex
+        eligendi quo, provident quae explicabo laborum itaque facere mollitia
+        molestiae? Maxime perspiciatis amet officia cupiditate veniam reiciendis
+        voluptatum illum aperiam consequatur odio, reprehenderit, sequi atque
+        velit quam quaerat porro distinctio? Nemo in hic aspernatur architecto
+        magnam cupiditate?
+      </p>
+    </article>
+  );
+}
+export default Nav;
